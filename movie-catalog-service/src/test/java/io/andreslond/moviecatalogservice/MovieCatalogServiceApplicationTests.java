ca00c32466c76f675effcc6cfc2692a5c5663f14
Andres Torres <andrestor2@gmail.com>
2023-02-23 07:26:42 -0500

    Initial state of 3 microservices.

---
+++ movie-catalog-service/src/test/java/io/andreslond/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.andreslond.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
