1032efb5cef916c824836b29b647c53311a0a527
Andres Torres <andrestor2@gmail.com>
2023-03-05 08:08:57 -0500

    FEAT: Added movie endpoint to get movie data. Also change properties to avoid port collision.

--- movie-info-service/src/main/java/io/andreslond/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/io/andreslond/movieinfoservice/models/Movie.java
@@ -1,4 +1,4 @@
-package models;
+package io.andreslond.movieinfoservice.models;
 
 public class Movie {
 
--- movie-info-service/src/main/java/io/andreslond/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/andreslond/movieinfoservice/resources/MovieResource.java
@@ -1,6 +1,6 @@
-package resources;
+package io.andreslond.movieinfoservice.resources;
 
-import models.Movie;
+import io.andreslond.movieinfoservice.models.Movie;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
