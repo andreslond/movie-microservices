16bd2d3fd603caf4762bcf864ebc208afcccdd95
Andres Torres <andrestor2@gmail.com>
2023-03-11 10:41:00 -0500

    FEAT: Added reactive programming method to perform the request.

--- movie-catalog-service/src/main/java/io/andreslond/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/andreslond/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +19,10 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webclientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -31,7 +35,13 @@ public class MovieCatalogResource {
         // For each movie Id, call movie info service and get movie details.
         return ratings.stream().map(rating -> {
 
-            Movie ratedMovie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId() , Movie.class);
+            //Movie ratedMovie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie ratedMovie = webclientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
 
             return new CatalogItem(ratedMovie.getName(), ratedMovie.getDirector(), rating.getRating());
         }).collect(Collectors.toList());
