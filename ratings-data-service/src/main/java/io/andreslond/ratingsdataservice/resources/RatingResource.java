d3947d99ac3e01aa77749b5e89aa88a4efc80790
Andres Torres <andrestor2@gmail.com>
2023-03-12 15:18:33 -0500

    FEAT: Added basic code to ratings endpoint.

--- ratings-data-service/src/main/java/io/andreslond/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/io/andreslond/ratingsdataservice/resources/RatingResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResource {
@@ -12,5 +15,13 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRatings(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+    @RequestMapping("users/{userId}")
+    public List<Rating> getRatedMoviesByUserId(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("Movie01", 5),
+                new Rating("Movie02", 1)
+        );
+        return ratings;
     }
 }
