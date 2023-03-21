470f1250b73fd595ab2c00b95471fb73ba3c7c7a
Andres Torres <andrestor2@gmail.com>
2023-03-21 07:25:16 -0500

    FEAT: Added code base to add an external api call to improve movie service.

--- movie-info-service/src/main/java/io/andreslond/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/andreslond/movieinfoservice/MovieInfoServiceApplication.java
@@ -1,10 +1,18 @@
 package io.andreslond.movieinfoservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieInfoServiceApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
--- movie-info-service/src/main/java/io/andreslond/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/andreslond/movieinfoservice/resources/MovieResource.java
@@ -1,13 +1,22 @@
 package io.andreslond.movieinfoservice.resources;
 
 import io.andreslond.movieinfoservice.models.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+
+    @Value("${movie.api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInformation(@PathVariable("movieId") String movieId) {
